d2ce956c4711f4a0a2e599cb46c5d672841310f7
ADIL BAINS <adibains06@gmail.com>
2025-10-26 19:19:12 +0530

    cors error handling

--- backend/app.js
+++ backend/app.js
@@ -26,13 +26,6 @@ main().then(()=>{
 
 const cors = require('cors');
 
-// Pre-flight handler
-app.options('*', cors({
-    origin: 'https://todo-application-v1-bains.vercel.app',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 // Main CORS middleware
 app.use(cors({
